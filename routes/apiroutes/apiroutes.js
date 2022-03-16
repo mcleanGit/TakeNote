74bd2282e388efe5cfcbd6f1b18ab70428000182
Don McLean <mclean@utoronto.ca>
2022-03-16 16:07:52 -0400

    routes -- NB these are to be rewritten post store.js creation

--- routes/apiroutes/apiroutes.js
+++ routes/apiroutes/apiroutes.js
@@ -1,8 +1,59 @@
+const router = require('express');
 const { route } = require('express/lib/application');
+const res = require('express/lib/response');
+const fs = require('fs');
 
-const router = require('express').Router();
-
-router.use(require('./apiroutes'));
-route.use(requre('./htmlroutes'));
+router.use(require('./routes/apiroutes/apiroutes.js'));
+router.use(requre('./routes/htmlroutes/htmlroutes'));
 
 module.exports = router;
+module.exports = function(app) {
+
+ app.get('/notes', function(req, res) {
+  fs.readFile('./db/notes.json', (err, data) => {
+   if (err) throw err;
+   dbNotes = JSON.parse(data);
+   res.send(dbNotes);
+  });
+});
+ app.post('/notes', function (req, res) {
+  const userNotes = req.body;
+  fs.readFile('/db/notes.json', (err, data) => {
+   if (err) throw err;
+   dbNotes = JSON.parse(data);
+   dbNotes.push(userNotes);
+   let number = 1;
+   dbNotes.forEach((note, index) => {
+    note.id = number;
+    number++;
+    return dbNotes;
+   });
+   stringNotes = JSON.stringify(dbNotes);
+
+   fs.writeFile('.db/notes.json', stringNotes, (err, data) => {
+    if (err) throw err; 
+   });
+  });
+ });
+
+  app.delete('api/notes/:id', function(req, res) {
+   const deleteNote = req.param.id;
+   fs.readFile ('./db/notes.json', (err, data) => {
+    if (err) throw err;
+
+    dbNotes = JSON.parse(data);
+    for (let i = 0; i < dbNotes.length; i++) {
+      if (dbNotes[i].id === Number(deleteNote)) {
+       dbNotes.splice([i], 1);
+      }
+    }
+    stringNotes = JSON.stringify(dbNotes);
+
+    fs.writeFile('./db/notes.json', stringNotes, (err, data) => {
+     if (err) throw err;
+    });
+     res.status(223).send();
+   });
+  });
+ }
+
--- routes/htmlroutes/htmlroutes.js
+++ routes/htmlroutes/htmlroutes.js
@@ -1,16 +1,17 @@
 const path = require('path');
 const router = require('express');
 
-router.get('/', (req, res) => {
+
+router.get('/notes', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+router.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
