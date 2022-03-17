9865a8d386407226d337eb3cd59a7e18ec824f5c
Don McLean <mclean@utoronto.ca>
2022-03-17 11:45:07 -0400

    moslty boilerplate, to revew PORT calls

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
 // server.js deals with routing calls
 const express = require('express');
-// const { notes } = require('./db/notes.json');
-
-// router.use(require('./routes/apiroutes/apiroutes.js'));
-// router.use(require('./routes/htmlroutes/htmlroutes.js'));
-const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get('/api/notes', (req, res) => {
- res.json(data);
-});
+const PORT = process.env.PORT || 3001;
 
-// const apiroutes = require('./routes/apiroutes/apiroutes.js');
-// const htmlroutes = require('./routes/htmlroutes/htmlroutes.js');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.directory('public'));
-// app.use(express.static('public'));
+require('./routes/apiroutes/apiroutes.js');
+require('./routes/htmlroutes/htmlroutes.js');
 
-// app.use('/api', apiroutes);
-// app.use('/', htmlroutes);
-
-app.listen(PORT, () => {
+app.listen(PORT, function() {
  console.log(`API server now on port ${PORT}!`);
 });
