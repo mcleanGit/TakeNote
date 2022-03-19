b7eae6e1dfbbf5ae0223c3c28908dc49280170a2
Don McLean <mclean@utoronto.ca>
2022-03-19 11:34:40 -0400

    revised pathways and required uuid - tbd

--- routes/apiroutes/apiroutes.js
+++ routes/apiroutes/apiroutes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const fs = require('fs');
-const store = require('./db/store.js');
+const store = require('../../db/store.js');
 // const server = require('./server.js');
+const app = require('express');
+const uuid = require('uuid'); 
 
 // to review this code and need for it in this form
 module.exports = function(app) {
  app.get('api/notes', function(req, res) {
-  fs.readFile('./db/db.json', (err, data) => {
+  fs.readFile('../../db/db.json', (err, data) => {
    if (err) throw err;
    dbNotes = JSON.parse(data);
    res.send(dbNotes);
@@ -14,7 +16,7 @@ module.exports = function(app) {
 });
  app.post('/api/notes', function (req, res) {
   const userNotes = req.body;
-  fs.readFile('./db/db.json', (err, data) => {
+  fs.readFile('../../db/db.json', (err, data) => {
    if (err) throw err;
    dbNotes = JSON.parse(data);
    dbNotes.push(userNotes);
@@ -26,7 +28,7 @@ module.exports = function(app) {
    });
    stringNotes = JSON.stringify(dbNotes);
 
-   fs.writeFile('.db/db.json', stringNotes, (err, data) => {
+   fs.writeFile('../../db/db.json', stringNotes, (err, data) => {
     if (err) throw err; 
    });
   });
@@ -34,7 +36,7 @@ module.exports = function(app) {
 
   app.delete('api/notes/:id', function(req, res) {
    const deleteNote = req.param.id;
-   fs.readFile ('./db/notes.json', (err, data) => {
+   fs.readFile ('../../db/db.json', (err, data) => {
     if (err) throw err;
 
     dbNotes = JSON.parse(data);
@@ -45,11 +47,11 @@ module.exports = function(app) {
     }
     stringNotes = JSON.stringify(data);
 
-    fs.writeFile('./db/notes.json', stringNotes, (err, data) => {
+    fs.writeFile('../../db/db.json', stringNotes, (err, data) => {
      if (err) throw err;
     });
      res.status(223).send();
    });
   });
- }
+}
 
