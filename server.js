e0383e790e7c82c8d7f8699cd3c75387a1c76339
Don McLean <mclean@utoronto.ca>
2022-03-21 13:15:52 -0400

    server.js functional

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 // server.js deals with routing calls
 const express = require('express');
 const app = express();
+const apiRoutes = require('./routes/apiroutes/apiroutes.js');
+const htmlRoutes = require('./routes/htmlroutes/htmlroutes.js');
 
 const PORT = process.env.PORT || 3001;
 
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-require('./routes/apiroutes/apiroutes.js');
-require('./routes/htmlroutes/htmlroutes.js');
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
+// working thus far
 app.listen(PORT, function() {
  console.log(`API server now on port ${PORT}!`);
 });
